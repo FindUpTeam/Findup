929608ba8341822bb68cb3d7dc2a35df2f7816a4
kazuhisanoguchi <pasuwaado135@gmail.com>
2020-12-15 15:23:20 +0200

    Firebase

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import store from './store'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import firebase from 'firebase'
 import i18n from './i18n'
 
 Vue.use(BootstrapVue)
@@ -14,7 +15,20 @@ Vue.use(BootstrapVueIcons)
 
 Vue.config.productionTip = false
 
+const config = {
+  apiKey: 'AIzaSyA-6d87mtxWv4nlHFrdsdTLUrLeHT5Vf7s',
+  authDomain: 'vue-firebase-sample-23acb.firebaseapp.com',
+  projectId: 'vue-firebase-sample-23acb',
+  storageBucket: 'vue-firebase-sample-23acb.appspot.com',
+  messagingSenderId: '830658520809',
+  appId: '1:830658520809:web:ddfe6b9211b2646d9e7278'
+}
+firebase.initializeApp(config)
+
 new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App },
   router,
   store,
   i18n,
