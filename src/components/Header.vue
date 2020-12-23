9d7ed72018b956289c71c0984425781c8c6fd9a7
thelowlyClutch <armandhrubo@gmail.com>
2020-12-23 08:49:47 +0200

    -Added dropdown and search bar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,17 +13,33 @@
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
+
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-5" placeholder="Search"></b-form-input>
+        </b-nav-form>
      
 
        <b-navbar-nav class="ml-auto">
-          <b-nav-item :to="`/${$i18n.locale}/login`">{{ $t('header.login') }}</b-nav-item>
-          <b-nav-item :to="`/${$i18n.locale}/signup`">{{ $t('header.signup') }}</b-nav-item>
+          
+
+          
+          <!--<b-nav-item :to="`/${$i18n.locale}/login`">{{ $t('header.login') }}</b-nav-item>
+          <b-nav-item :to="`/${$i18n.locale}/signup`">{{ $t('header.signup') }}</b-nav-item>-->
   
         </b-navbar-nav>
           
         <b-nav-item-dropdown text="Lang" right>
           <b-dropdown-item  @click="setLocale('en')">ENG</b-dropdown-item>
           <b-dropdown-item  @click="setLocale('et')">EST</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em> <!-- Retrieve data and display the name of the logged in user -->
+          </template>
+          <b-dropdown-item href=" ">Settings</b-dropdown-item> <!-- add the relevant codes for the respective pages -->
+          <b-dropdown-item href=" ">Sign Out</b-dropdown-item> <!-- same as above -->
         </b-nav-item-dropdown>
 
         
